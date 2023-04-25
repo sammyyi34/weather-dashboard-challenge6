ea5016a5683e95a1739c9c733f7be6d7982a6543
Sammy <sammyhyi@gmail.com>
2023-04-25 15:55:49 -0400

    last search does not show when user does a new search

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,27 @@ var currentWind = document.getElementById('current-wind');
 var currentHumidity = document.getElementById('current-humidity');
 localStorage.removeItem("cityName");
 
+var day1IconEl = document.getElementById('day1-icon');
+var day1TempEl = document.getElementById('day1-temp');
+var day1WindEl = document.getElementById('day1-wind');
+var day1HumidityEl = document.getElementById('day1-humidity');
+var day2IconEl = document.getElementById('day2-icon');
+var day2TempEl = document.getElementById('day2-temp');
+var day2WindEl = document.getElementById('day2-wind');
+var day2HumidityEl = document.getElementById('day2-humidity');
+var day3IconEl = document.getElementById('day3-icon');
+var day3TempEl = document.getElementById('day3-temp');
+var day3WindEl = document.getElementById('day3-wind');
+var day3HumidityEl = document.getElementById('day3-humidity');
+var day4TempEl = document.getElementById('day4-temp');
+var day4WindEl = document.getElementById('day4-wind');
+var day4HumidityEl = document.getElementById('day4-humidity');
+var day4IconEl = document.getElementById('day4-icon');
+var day5IconEl = document.getElementById('day5-icon');
+var day5TempEl = document.getElementById('day5-temp');
+var day5WindEl = document.getElementById('day5-wind');
+var day5HumidityEl = document.getElementById('day5-humidity');
+
 // displays current date of the city the user searched
 function displayTime() {
   var cityName = getFromLocal();
@@ -67,6 +88,26 @@ function todayWeatherInfo() {
     currentWind.innerHTML += ' ' + data.wind.speed + ' MPH';
     currentHumidity.innerHTML += ' ' + data.main.humidity + '%';
     getFiveDay(cityName)
+    day1IconEl.src = '';
+    day1TempEl.innerHTML = 'Temp:';
+    day1WindEl.innerHTML = 'Wind:';
+    day1HumidityEl.innerHTML = 'Humidity:';
+    day2IconEl.src = '';
+    day2TempEl.innerHTML = 'Temp:';
+    day2WindEl.innerHTML = 'Wind:';
+    day2HumidityEl.innerHTML = 'Humidity:';
+    day3IconEl.src = '';
+    day3TempEl.innerHTML = 'Temp:';
+    day3WindEl.innerHTML = 'Wind:';
+    day3HumidityEl.innerHTML = 'Humidity:';
+    day4IconEl.src = '';
+    day4TempEl.innerHTML = 'Temp:';
+    day4WindEl.innerHTML = 'Wind:';
+    day4HumidityEl.innerHTML = 'Humidity:';
+    day5IconEl.src = '';
+    day5TempEl.innerHTML = 'Temp:';
+    day5WindEl.innerHTML = 'Wind:';
+    day5HumidityEl.innerHTML = 'Humidity:';
   })
 }
 
@@ -89,10 +130,7 @@ function getFiveDay(cityName) {
     var day1Date = fiveDayForecast[0].dt_txt.slice(0, 10)
     day1Date = dayjs().format('MM/DD/YYYY');
     day1DateEl.textContent = day1Date;
-    var day1IconEl = document.getElementById('day1-icon');
-    var day1TempEl = document.getElementById('day1-temp');
-    var day1WindEl = document.getElementById('day1-wind');
-    var day1HumidityEl = document.getElementById('day1-humidity');
+
     // gets the icon for weather condition that day
     var day1Icon = fiveDayForecast[0].weather[0].icon
     day1IconEl.src = `https://openweathermap.org/img/wn/${day1Icon}@2x.png`
@@ -106,10 +144,6 @@ function getFiveDay(cityName) {
     var day2Date = fiveDayForecast[1].dt_txt.slice(0, 10)
     day2Date = dayjs(day2Date).format('MM/DD/YYYY');
     day2DateEl.textContent = day2Date;
-    var day2IconEl = document.getElementById('day2-icon');
-    var day2TempEl = document.getElementById('day2-temp');
-    var day2WindEl = document.getElementById('day2-wind');
-    var day2HumidityEl = document.getElementById('day2-humidity');
     var day2Icon = fiveDayForecast[1].weather[0].icon
     day2IconEl.src = `https://openweathermap.org/img/wn/${day2Icon}@2x.png`
     day2TempEl.innerHTML += ' ' + fiveDayForecast[1].main.temp + '°F';
@@ -121,10 +155,6 @@ function getFiveDay(cityName) {
     var day3Date = fiveDayForecast[2].dt_txt.slice(0, 10);
     day3Date = dayjs(day3Date).format('MM/DD/YYYY');
     day3DateEl.textContent = day3Date;
-    var day3IconEl = document.getElementById('day3-icon');
-    var day3TempEl = document.getElementById('day3-temp');
-    var day3WindEl = document.getElementById('day3-wind');
-    var day3HumidityEl = document.getElementById('day3-humidity');
     var day3Icon = fiveDayForecast[2].weather[0].icon
     day3IconEl.src = `https://openweathermap.org/img/wn/${day3Icon}@2x.png`
     day3TempEl.innerHTML += ' ' + fiveDayForecast[2].main.temp + '°F';
@@ -136,25 +166,17 @@ function getFiveDay(cityName) {
     var day4Date = fiveDayForecast[3].dt_txt.slice(0, 10);
     day4Date = dayjs(day4Date).format('MM/DD/YYYY');
     day4DateEl.textContent = day4Date;
-    var day4TempEl = document.getElementById('day4-temp');
-    var day4WindEl = document.getElementById('day4-wind');
-    var day4HumidityEl = document.getElementById('day4-humidity');
-    var day4IconEl = document.getElementById('day4-icon');
     var day4Icon = fiveDayForecast[3].weather[0].icon
     day4IconEl.src = `https://openweathermap.org/img/wn/${day4Icon}@2x.png`
     day4TempEl.innerHTML += ' ' + fiveDayForecast[3].main.temp + '°F';
     day4WindEl.innerHTML += ' ' + fiveDayForecast[3].wind.speed + ' MPH';
     day4HumidityEl.innerHTML += ' ' + fiveDayForecast[3].main.humidity + '%';
 
-    // day 2 info
+    // day 5 info
     var day5DateEl = document.getElementById('day5')
     var day5Date = fiveDayForecast[4].dt_txt.slice(0, 10);
     day5Date = dayjs(day5Date).format('MM/DD/YYYY');
     day5DateEl.textContent = day5Date;
-    var day5IconEl = document.getElementById('day5-icon');
-    var day5TempEl = document.getElementById('day5-temp');
-    var day5WindEl = document.getElementById('day5-wind');
-    var day5HumidityEl = document.getElementById('day5-humidity');
     var day5Icon = fiveDayForecast[4].weather[0].icon
     day5IconEl.src = `https://openweathermap.org/img/wn/${day5Icon}@2x.png`
     day5TempEl.innerHTML += ' ' + fiveDayForecast[4].main.temp + '°F';
